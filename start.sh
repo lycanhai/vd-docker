283c9c7ad8ca572513354ac083c69ec47eb03106
lycanhai <kythuatmda1@gmail.com>
2025-02-28 19:28:32 +0700

    start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Khởi chạy ngrok
-./ngrok http 5678 --log=stdout --region=auto &
+/usr/local/bin/ngrok http 5678 --log=stdout --region=auto &
 
 # Khởi chạy n8n
-n8n start --tunnel
+/app/node_modules/.bin/n8n start --tunnel
